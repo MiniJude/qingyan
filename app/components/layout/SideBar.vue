581dc4d3cc44a3a5cae54324bf314ce297c9d647
minijude <wc1546985690@gmail.com>
2025-03-19 18:07:52 +0800

    feat: 更新 UnoCSS 配置，优化侧边栏布局，新增存储使用组件

--- app/components/layout/SideBar.vue
+++ app/components/layout/SideBar.vue
@@ -1,17 +1,26 @@
 <template>
-  <div bg-red w-242px>
+  <div class="side-bar" flex flex-col w-242px>
     <!-- logo -->
     <div flex gap-2 h-69px items-center p="l-13px">
       <div i-carbon:logo-kubernetes text="primary 48px" />
       <span text="primary font-bold 28px">清研</span>
     </div>
-    <!-- 菜单 -->
-    <div p="t-22px r-16px b-6px l-15px" />
+
+    <div flex flex-1 flex-col p="t-13px r-16px b-6px l-15px">
+      <!-- 空间切换 -->
+      <div m="l-1px b-12px t-10px" flex h-35px>
+        <div text="white 10px" flex-center rounded-5px bg-primary h-36px w-36px>
+          数智<br>空间
+        </div>
+        <span m="l-21px" flex-1>个人空间</span>
+      </div>
+    </div>
+    <LayoutStorageUsageBox />
   </div>
 </template>
 
 <style lang="scss" scoped>
-.nav-bg {
+.side-bar {
   background-color: #edeff3;
 }
 </style>
---
+++ app/components/layout/StorageUsageBox.vue
@@ -0,0 +1,15 @@
+<template>
+  <div h-78px>
+    <div>
+      <span>空间容量</span>
+      <span>
+        设置
+      </span>
+    </div>
+    <el-progress :text-inside="true" :stroke-width="26" :percentage="70" />
+    <div>
+      <span>4.5G/10G</span>
+      <span>扩容</span>
+    </div>
+  </div>
+</template>
