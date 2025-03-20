35df17bc979e875060a7c79260bafc18312101c8
minijude <wc1546985690@gmail.com>
2025-03-20 22:12:51 +0800

    feat: 更新样式文件，新增手机图标，优化登录页面表单结构和验证规则

--- app/layouts/sys.vue
+++ app/layouts/sys.vue
@@ -2,7 +2,7 @@
 import { CircleCloseFilled } from '@element-plus/icons-vue'
 
 const route = useRoute()
-const title = computed(() => route.meta.title as string)
+const title = computed(() => route.meta.title)
 </script>
 
 <template>
@@ -10,11 +10,19 @@ const title = computed(() => route.meta.title as string)
     <div class="sys-layout" m-auto rounded-10px flex-center h-850px w-1351px relative>
       <img src="@/assets/img/logo.png" alt="logo" h-64px w-134px left-25px top-26px absolute>
       <CircleCloseFilled color="primary" h-40px w-40px cursor-pointer right-35px top-38px absolute />
-      <div class="sys-form-container" rounded-10px bg-white max-w-200px min-h-200px>
-        <h1 text-24px font-bold>
+
+      <div relative>
+        <div class="dot" left--10px top-20px style="background: #6CB5EE;" />
+        <div class="dot" bottom-20px right--10px style="background: #25DFE2;" />
+        <div class="dot" bottom-55px right--10px style="background: #8F67E8;" />
+        <div class="dot" bottom-90px right--10px style="background: #1A61FA;" />
+        <div class="rounded-[0_0_30px_30px] translate-x-[-50%] left-[50%] absolute z-2" flex-center text="14px white" h-35px w-172px style="background: #660874;">
           {{ title }}
-        </h1>
-        <slot />
+        </div>
+
+        <div class="sys-form-container" rounded-10px bg-white min-h-200px min-w-200px relative>
+          <slot />
+        </div>
       </div>
     </div>
   </main>
@@ -33,5 +41,15 @@ const title = computed(() => route.meta.title as string)
   box-shadow:
     0px 3px 6px rgba(0, 0, 0, 0.16),
     inset 0px 3px 6px rgba(0, 0, 0, 0.16);
+  z-index: 1;
+}
+
+.dot {
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  background-color: #000;
+  border-radius: 50%;
+  z-index: 0;
 }
 </style>
--- app/pages/sys/logon.vue
+++ app/pages/sys/logon.vue
@@ -4,20 +4,20 @@ import { ref } from 'vue'
 
 definePageMeta({
   layout: 'sys',
-  title: '登录',
+  title: '输入手机号',
 })
 
 const form = ref({
-  username: '',
-  password: '',
+  phone: '',
+  code: '',
 })
 
 const rules = ref({
-  username: [
-    { required: true, message: '请输入用户名', trigger: 'blur' },
+  phone: [
+    { message: '请输入手机号', trigger: 'blur' },
   ],
-  password: [
-    { required: true, message: '请输入密码', trigger: 'blur' },
+  code: [
+    { message: '请输入验证码', trigger: 'blur' },
   ],
 })
 
@@ -33,11 +33,52 @@ function handleSubmit() {
 </script>
 
 <template>
-  <div>
-    <el-form ref="formRef" :model="form" :rules="rules" label-width="100px">
-      <el-form-item label="用户名" prop="username">
-        <el-input v-model="form.username" placeholder="请输入用户名" />
+  <div w-450px p="t-48px b-40px r-65px l-65px">
+    <el-form ref="formRef" class="w-320px" :model="form" :rules="rules" label-width="100px" label-position="top">
+      <el-form-item label="手机号" prop="phone">
+        <el-input v-model="form.phone">
+          <template #suffix>
+            <SvgoPhone text-tprimary />
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item label="验证码" prop="code">
+        <div flex="~ gap-15px" w-full>
+          <el-input v-model="form.code" class="flex-1" />
+          <el-button type="primary" class="!h-45px !w-119px">
+            获取验证码
+          </el-button>
+        </div>
+      </el-form-item>
+      <el-form-item class="!mt-45px">
+        <el-button type="primary" class="mx-auto block !h-40px !w-102px">
+          完成
+        </el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
+
+<style lang="scss" scoped>
+:deep(.el-form-item) {
+  margin-bottom: 0;
+  margin-top: 20px;
+
+  .el-form-item__label {
+    margin-bottom: 10px;
+  }
+
+  .el-input {
+    .el-input__wrapper {
+      background-color: #c9cdd4;
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+
+    .el-input__inner {
+      height: 45px;
+      line-height: 45px;
+    }
+  }
+}
+</style>
