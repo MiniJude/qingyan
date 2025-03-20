1f6e4d02f239dca8f56a5e14759e8b4f745785f8
minijude <wc1546985690@gmail.com>
2025-03-20 23:44:20 +0800

    feat: 新增手机号登录页面，更新登录页面表单结构，添加二维码和微信图标

---
+++ app/pages/sys/logon-phone.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import type { FormInstance } from 'element-plus'
+import { ref } from 'vue'
+
+definePageMeta({
+  layout: 'sys',
+  title: '输入手机号',
+})
+
+const form = ref({
+  phone: '',
+  code: '',
+})
+
+const rules = ref({
+  phone: [
+    { message: '请输入手机号', trigger: 'blur' },
+  ],
+  code: [
+    { message: '请输入验证码', trigger: 'blur' },
+  ],
+})
+
+const formRef = ref<FormInstance>()
+
+function handleSubmit() {
+  formRef.value?.validate((valid) => {
+    if (valid) {
+      console.log('submit!')
+    }
+  })
+}
+</script>
+
+<template>
+  <div w-450px p="t-48px b-40px r-65px l-65px">
+    <el-form ref="formRef" class="w-320px" :model="form" :rules="rules" label-width="100px" label-position="top">
+      <el-form-item label="手机号" prop="phone">
+        <el-input v-model="form.phone">
+          <template #suffix>
+            <SvgoPhone text-tprimary />
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item label="验证码" prop="code">
+        <div flex="~ gap-15px" w-full>
+          <el-input v-model="form.code" class="flex-1" />
+          <el-button type="primary" class="!h-45px !w-119px">
+            获取验证码
+          </el-button>
+        </div>
+      </el-form-item>
+      <el-form-item class="!mt-45px">
+        <el-button type="primary" class="mx-auto block !h-40px !w-102px">
+          完成
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+:deep(.el-form-item) {
+  margin-bottom: 0;
+  margin-top: 20px;
+
+  .el-form-item__label {
+    margin-bottom: 10px;
+  }
+
+  .el-input {
+    .el-input__wrapper {
+      background-color: #c9cdd4;
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+
+    .el-input__inner {
+      height: 45px;
+      line-height: 45px;
+    }
+  }
+}
+</style>
--- app/pages/sys/logon.vue
+++ app/pages/sys/logon.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
 import type { FormInstance } from 'element-plus'
+import { User } from '@element-plus/icons-vue'
 import { ref } from 'vue'
 
 definePageMeta({
   layout: 'sys',
-  title: '输入手机号',
+  title: '登录账户',
 })
 
 const form = ref({
-  phone: '',
-  code: '',
+  account: '',
+  password: '',
 })
 
 const rules = ref({
-  phone: [
-    { message: '请输入手机号', trigger: 'blur' },
+  account: [
+    { message: '请输入登录账户', trigger: 'blur' },
   ],
-  code: [
-    { message: '请输入验证码', trigger: 'blur' },
+  password: [
+    { message: '请输入登录密码', trigger: 'blur' },
   ],
 })
 
@@ -33,36 +34,58 @@ function handleSubmit() {
 </script>
 
 <template>
-  <div w-450px p="t-48px b-40px r-65px l-65px">
-    <el-form ref="formRef" class="w-320px" :model="form" :rules="rules" label-width="100px" label-position="top">
-      <el-form-item label="手机号" prop="phone">
-        <el-input v-model="form.phone">
+  <div p="r-60px" flex="~ gap-5px" w-650px items-center>
+    <div flex="~ col items-center" w-265px>
+      <img src="@/assets/img/qrcode.png" alt="wechat" w-145px>
+      <div mt-15px pl-7px flex w-145px items-center>
+        <img src="@/assets/img/wechat.png" alt="wechat" w-21px>
+        <span text="12px tprimary" ml-27px>清研智库</span>
+      </div>
+      <p mt-5px text="12px tprimary" opacity-80>
+        获取最新资讯动态和精彩活动
+      </p>
+    </div>
+    <el-form
+      ref="formRef" class="pb-40px pt-83px w-320px" :model="form" :rules="rules" label-width="100px"
+      label-position="top"
+    >
+      <el-form-item label="账号" prop="account">
+        <el-input v-model="form.account" autocomplete="off">
           <template #suffix>
-            <SvgoPhone text-tprimary />
+            <el-icon>
+              <User />
+            </el-icon>
           </template>
         </el-input>
       </el-form-item>
-      <el-form-item label="验证码" prop="code">
-        <div flex="~ gap-15px" w-full>
-          <el-input v-model="form.code" class="flex-1" />
-          <el-button type="primary" class="!h-45px !w-119px">
-            获取验证码
-          </el-button>
-        </div>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="form.password" type="password" show-password />
       </el-form-item>
-      <el-form-item class="!mt-45px">
+      <p text="12px tprimary" text-right opacity-90 cursor-pointer>
+        忘记密码
+      </p>
+      <el-form-item class="!mb-0 !mt-30px">
         <el-button type="primary" class="mx-auto block !h-40px !w-102px">
-          完成
+          下一步
         </el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
+<style></style>
+
 <style lang="scss" scoped>
+:deep(.el-input) {
+  input:-webkit-autofill {
+    background-color: white !important;
+    color: black !important;
+    transition: background-color 5000s ease-in-out 0s;
+  }
+}
+
 :deep(.el-form-item) {
-  margin-bottom: 0;
-  margin-top: 20px;
+  margin-bottom: 20px;
 
   .el-form-item__label {
     margin-bottom: 10px;
@@ -78,6 +101,15 @@ function handleSubmit() {
     .el-input__inner {
       height: 45px;
       line-height: 45px;
+    }
+
+    .el-input__suffix {
+      .el-input__suffix-inner {
+        .el-icon {
+          color: var(--colors-tprimary);
+          font-size: 18px;
+        }
+      }
     }
   }
 }
